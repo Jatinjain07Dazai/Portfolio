5b702df184e2af0119c91edd7d4d45ce01ddba13
Jatinjain07Dazai <72065165+Jatinjain07Dazai@users.noreply.github.com>
2022-02-15 13:19:35 +0530

    almost there

--- br.js
+++ br.js
@@ -1,3 +1,58 @@
+const tog = document.getElementById("op");
+const hd = document.getElementById("hd");
+const l1 = document.getElementById("1");
+const l2 = document.getElementById("2");
+const l3 = document.getElementById("3");
+const nav =  document.getElementsByClassName("nav-link");
+var a = 0;
+
+for(i = 0; i < nav.length; i++){
+	nav[i].addEventListener("click", function(){
+	openheader(a)
+	a += 1
+	})
+}
+
+
+
+function openheader(s){
+	if(s % 2 == 0){
+		hd.style.display ="block";
+		l1.style.position = "absolute";
+		l2.style.position ="absolute";
+		l1.style.top ="20%";
+		l2.style.top ="20%";
+		l1.style.transform = "rotate(140deg)";
+		l1.style.transition = "all .40s ease-in";
+		l2.style.transform = "rotate(-140deg)";
+		l2.style.transition = "all .40s ease-in";
+		l3.style.display = "none";
+	}
+	else{
+		hd.style.display ="none";
+		l1.style.position = "";
+		l2.style.position ="";
+		l1.style.top ="";
+		l2.style.top ="";
+		l1.style.transform = "rotate(0deg)";
+		l1.style.transition = "all .40s ease-in";
+		l2.style.transform = "rotate(0deg)";
+		l2.style.transition = "all .40s ease-in";
+		l3.style.display = " block";
+}
+}
+
+
+
+tog.addEventListener("click", function()  {
+	openheader(a);
+	a += 1
+})
+
+
+
+
+
 function validate(){
 	let name = document.getElementById('name').value;
 	let email = document.getElementById('email').value;
@@ -21,6 +76,4 @@ function validate(){
 		err.style.color = "red";
 		return false;
 	}
-
-
 }
